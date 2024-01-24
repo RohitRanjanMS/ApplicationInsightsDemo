9bdb366f4886932908697a8e5ad430d6f505f725
Rohit Ranjan <90008725+RohitRanjanMS@users.noreply.github.com>
2024-01-24 01:41:54 -0800

    Added example for isolated.

---
+++ Isolated/IsolatedApplicationInsightsDemo/CustomTelemetryProcessor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+namespace IsolatedApplicationInsightsDemo
+{
+    using Microsoft.ApplicationInsights.Channel;
+    using Microsoft.ApplicationInsights.DataContracts;
+    using Microsoft.ApplicationInsights.Extensibility;
+
+    public class CustomTelemetryProcessor : ITelemetryProcessor
+    {
+        private ITelemetryProcessor Next { get; set; }
+
+        // Link processors to each other in the chain.
+        public CustomTelemetryProcessor(ITelemetryProcessor next)
+        {
+            this.Next = next;
+        }
+
+        public void Process(ITelemetry telemetry)
+        {
+            if (telemetry is DependencyTelemetry dependency && dependency.Name == "Invoke")
+            {
+                // Do not send telemetry for the "Invoke" dependency.
+                return;
+            }
+            // Call the next processor in the chain.
+            this.Next.Process(telemetry);
+        }
+    }
+}
---
+++ Isolated/IsolatedApplicationInsightsDemo/Program.cs
@@ -0,0 +1,31 @@
+using IsolatedApplicationInsightsDemo;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        // Add your custom telemetry processor here.
+        //services.AddApplicationInsightsTelemetryProcessor<CustomTelemetryProcessor>();
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.Configure<LoggerFilterOptions>(options =>
+        {
+            // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+            // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+            LoggerFilterRule toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+            if (toRemove is not null)
+            {
+                options.Rules.Remove(toRemove);
+            }
+        });
+    })
+    .Build();
+
+host.Run();
